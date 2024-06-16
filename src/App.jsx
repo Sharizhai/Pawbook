bf5795137feba57e5a9919e9477e909a543bf45e
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-06-16 21:32:46 +0200

    [Home page]Mobile vue

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import Button from './components/Button';
-//import HomePage from './routes/HomePage';
+import HomePage from './routes/HomePage';
 
 const App = () => {
     return  (
     <>
-        <h1 className="website-name">Pawbook</h1>
-        <Button 
-                        label="Cliquez-moi !" 
-                        onClick={() => console.log('Button clicked!')} 
-                    />
+        <HomePage />
     </>
     );
 };
---
+++ src/routes/HomePage.jsx
@@ -0,0 +1,39 @@
+import '../css/HomePage.css';
+import '../css/global.css';
+
+import Button from '../components/Button';
+
+const HomePage = () => {
+
+    return (
+        <>
+            <main className="main-container">
+                <header className="header-container">
+                    <img src="Logo_Pawbook.png" alt="Logo du site Pawbook" className="logo" />
+                    <h1 className="website-name logo-title">Pawbook</h1>
+                    <p className="subheading">Le premier réseau social pour les amoureux des animaux</p>
+                </header>
+
+                <nav className="buttons-container">
+                    <Button
+                        label="Se connecter"
+                        onClick={() => console.log('Je me connecte')}
+                    />
+
+                    <Button
+                        label="Créer un compte"
+                        onClick={() => console.log('Je me cée un compte')}
+                    />
+                </nav>
+            </main>
+
+            <footer className="footer-container">
+                <a href="/cgu">Conditions Générales d'Utilisation</a>
+                <p>&copy; 2024 Pawbook</p>
+            </footer>
+        </>
+    )
+
+};
+
+export default HomePage;
