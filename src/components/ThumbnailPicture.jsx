951c8930c8a5007fc1963551cd0632df0a6fc1f8
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-08-08 11:45:28 +0200

    [thumbnail]Add logic to enlarge images

--- src/components/ThumbnailPicture.jsx
+++ src/components/ThumbnailPicture.jsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/ThumbnailPicture.css';
-import Post_image from "../assets/dog-3724261_640.jpg"
-import Post_image_2 from "../assets/dog-4072161_640.jpg"
+import Post_image from "../assets/dog-3724261_640.jpg";
+import Post_image_2 from "../assets/dog-4072161_640.jpg";
+import { handleImageClick, handleCloseEnlargedImage } from "../utils/imageUtils";
 
 const ThumbnailPicture = () => {
-    return (
-        <>
-            <div className="thumbnail-picture-container">
-                <img src={Post_image_2} alt="Photo de " className="thumbnail-picture" />
+  const [enlargedImage, setEnlargedImage] = useState(null);
 
-                <button className="thumbnail-delete-button">
-                    <span className="material-symbols-outlined">
-                        delete
-                    </span>
-                </button>
-            </div>
-        </>
-    );
+  const handleThumbnailClick = () => {
+    handleImageClick(setEnlargedImage, Post_image_2);
+  };
+
+  const handleThumbnailDeleteClick = (e) => {
+    e.stopPropagation();
+    //TODO
+    // Ajouter la logique pour la suppression
+  };
+
+  const EnlargedImage = ({ src, onClose }) => (
+    <div className="enlarged-image-overlay" onClick={onClose}>
+      <button className="close-button" onClick={(e) => { e.stopPropagation(); onClose(); }}>
+        Fermer
+        <span className="material-symbols-outlined">close</span>
+      </button>
+      <img src={src} alt="Image agrandie" onClick={(e) => e.stopPropagation()} />
+    </div>
+  );
+
+  return (
+    <>
+      <div className="thumbnail-picture-container" onClick={handleThumbnailClick}>
+        <img src={Post_image_2} alt="Photo de " className="thumbnail-picture" />
+        <button className="thumbnail-delete-button" onClick={handleThumbnailDeleteClick}>
+          <span className="material-symbols-outlined">delete</span>
+        </button>
+      </div>
+      {enlargedImage && (
+        <EnlargedImage src={enlargedImage} onClose={() => handleCloseEnlargedImage(setEnlargedImage)} />
+      )}
+    </>
+  );
 };
 
 export default ThumbnailPicture;
