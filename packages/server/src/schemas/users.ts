e93a919c67f5cb00af5890934cfe0c6c77d9c3e4
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-09-13 17:02:23 +0200

    Clean old schemas + update follower interface + update users schema

--- packages/server/src/schemas/users.ts
+++ packages/server/src/schemas/users.ts
@@ -3,10 +3,19 @@ import { IUser } from "../types/IUser";
 
 const userSchema: Schema = new Schema({
     name: { type: String, required: true },
-    firstname: { type: String, required: true },
+    firstName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, select: false },
-    role: { type: String, required: true}
+    role: { type: String, enum: ["USER", "ADMIN"], required: true },
+    profilePicture: { type: String, required: false},
+    profileDescription: { type: String, required: false},
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post", required: false }],
+    animals: [{ type: Schema.Types.ObjectId, ref: "Animal", required: false }],
+    follows: [{ type: Schema.Types.ObjectId, ref: "Follow", required: false }],
+    followers: [{ type: Schema.Types.ObjectId, ref: "Follower", required: false }]
+}, 
+{ 
+    timestamps: true 
 });
 
 export default mongoose.model<IUser>('User', userSchema);
--- packages/server/src/types/IFollower.d.ts
+++ packages/server/src/types/IFollower.d.ts
@@ -4,6 +4,6 @@ import { IUser } from "./IUser";
 
 export interface IFollower extends Document {
     userID: Types.ObjectId;  // L'utilisateur qui est suivi
-    followingUser: IUser;  // L'utilisateur qui suit
+    followerUser: IUser;  // L'utilisateur qui suit
     createdAt: Date;
 }
