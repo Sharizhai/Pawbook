f4a7f297c1144ed193029178a9465d746598b027
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-09-13 22:29:37 +0200

    schéma follower

--- packages/server/src/schemas/follows.ts
+++ packages/server/src/schemas/follows.ts
@@ -6,10 +6,11 @@ const followSchema: Schema = new Schema({
     followedUser: { type: Schema.Types.ObjectId, ref: "User", required: true },
 }, 
 { 
+    // Pour le timestamp on ne veut pas récupérer de date de modification donc on précise le updateAt en false.
     timestamps: { createdAt: true, updatedAt: false } 
 });
 
-// On ajoute un index composé pour éviter les doublons
+// On ajoute un index composé pour éviter les doublons : un user ne peut suivre un autre un autre user qu'une seule fois : 
 followSchema.index({ userID: 1, followedUser: 1 }, { unique: true });
 
 export default mongoose.model<IFollow>("Follow", followSchema);
