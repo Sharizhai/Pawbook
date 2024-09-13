df569ead38d9d48a19bfeb3088d130bf1a335173
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-09-13 22:57:34 +0200

    clean up models

--- packages/server/src/models/index.ts
+++ packages/server/src/models/index.ts
@@ -1,26 +1,6 @@
-import { findAllCountry, findCountryById, createCountry, deleteCountry, updateCountry } from "./countryModel";
-import { getAllLocations, findLocationById, findLocationByCountryId, createLocation, deleteLocation, updateLocation } from "./locationsModel";
-import { getAllAccesses, findAccessById, findAccessesByCountryId, findAccessesByLocationId, findAccessesByLocationAndCountryId, deleteAccess, updateAccess, createAccess } from "./accessModel";
 import { getAllUsers, findUserById, createUser, deleteUser, updateUser, findByCredentials } from "./usersModel";
 
 export default {
-    "country" : {
-        "get" : findAllCountry,
-        "where" : findCountryById,
-        "create" : createCountry,
-        "delete": deleteCountry,
-        "update": updateCountry,
-    },
-    
-    "locations" : {
-        "get" : getAllLocations,
-        "where" : findLocationById,
-        "fromWhere" : findLocationByCountryId, 
-        "create" : createLocation,
-        "delete": deleteLocation,
-        "update": updateLocation,
-    },
-
     "users" : {
         "get" : getAllUsers,
         "where" : findUserById,
@@ -29,15 +9,4 @@ export default {
         "update": updateUser,
         "find" : findByCredentials
     },
-
-    "access" : {
-        "get" : getAllAccesses,
-        "where" : findAccessById,
-        "whereCountry" : findAccessesByCountryId,
-        "whereLocation" : findAccessesByLocationId,
-        "whereCountryLocation" : findAccessesByLocationAndCountryId,
-        "create" : createAccess,
-        "delete" : deleteAccess,
-        "update" : updateAccess,
-    }
 }
