522d38c37bc89de76a844e3882963d2e8e992f7d
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-09-17 21:35:29 +0200

    Add commentsModel

---
+++ packages/server/src/models/commentsModel.ts
@@ -0,0 +1,149 @@
+import { Response } from "express";
+import { Types } from "mongoose";
+import { z } from "zod";
+
+import { APIResponse } from "../utils/responseUtils";
+
+import Comment from "../schemas/comments";
+
+import { IComment } from "../types/IComment";
+
+//CRUD to get all comments
+export const getAllComments = async (response: Response): Promise<IComment[]> => {
+    try {
+        const comments = await Comment.find().select("authorId postId animalId").exec();
+
+        APIResponse(response, comments, "Liste de tous les commentaires récupérée avec succès");
+        return comments;
+    } catch (error) {
+        console.error(error);
+
+        APIResponse(response, null, "Erreur lors de la récupération de la liste des commentaires", 500);
+        return [];
+    }
+};
+
+//CRUD to get a comment from its id
+export const findCommentById = async (id: Types.ObjectId, response: Response): Promise<{ comment: IComment } | null> => {
+    try {
+        const comment = await Comment.findById(id).populate([
+            {
+                path: "authorId",
+                select: "name email"
+            },
+            {
+                path: "postId",
+                select: "authorId textContent photoContent comments"
+            }
+        ]).exec();
+
+        if (!comment) {
+            APIResponse(response, null, "Commentaire non trouvé", 404);
+            return null;
+        }
+
+        const result = { comment: comment.toObject() };
+
+        APIResponse(response, result, "Commentaire trouvé");
+        return result;
+    } catch (error: any) {
+        console.error(error);
+        APIResponse(response, null, "Erreur lors de la recherche du commentaire", 500);
+        return null;
+    }
+};
+
+//CRUD to create a new comment
+export const createComment = async (comment: Partial<IComment>, response: Response): Promise<IComment | null> => {
+    try {
+        const newComment = await Comment.create(comment);
+
+        APIResponse(response, newComment, "Commentaire ajouté avec succès", 201);
+        return newComment;
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            APIResponse(response, error.errors, "Données du commentaire invalides", 400);
+        } else {
+            console.error(error);
+            APIResponse(response, null, "Erreur lors de l'ajout du commentaire", 500);
+        }
+        return null;
+    }
+};
+
+//CRUD to delete a comment by its id
+export const deleteComment = async (id: Types.ObjectId, authorId: Types.ObjectId, response: Response): Promise<{ deletedCount: number }> => {
+    try {
+        const result = await Comment.deleteOne({ _id: id, authorId });
+
+        if (result.deletedCount === 0) {
+            APIResponse(response, null, "Commentaire non trouvé", 404);
+        } else {
+            APIResponse(response, result, "Commentaire supprimé avec succès");
+        }
+        return result;
+    } catch (error) {
+        console.error(error);
+        APIResponse(response, null, "Erreur lors de la suppression du commentaire", 500);
+        return { deletedCount: 0 };
+    }
+};
+
+//CRUD to update a comment by its id
+export const updateComment = async (id: Types.ObjectId, commentData: Partial<IComment>, response: Response): Promise<IComment | null> => {
+    try {
+        const updateComment = await Comment.findByIdAndUpdate(id, commentData, { new: true }).exec();
+
+        if (!updateComment) {
+            APIResponse(response, null, "Commentaire non trouvé", 404);
+            return null;
+        }
+        APIResponse(response, updateComment, "Commentaire mis à jour avec succès");
+        return updateComment;
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            APIResponse(response, error.errors, "Données invalides", 400);
+        } else {
+            console.error(error);
+            APIResponse(response, null, "Erreur lors de la mise à jour du commentaire", 500);
+        }
+        return null;
+    }
+};
+
+
+//CRUD to get all comments by their user ID
+export const findCommentsByAuthorId = async (authorId: Types.ObjectId, response: Response): Promise<IComment[] | null> => {
+    try {
+        const comments = await Comment.find({ authorId }).exec();
+
+        if (comments.length === 0) {
+            APIResponse(response, null, "Aucun commentaire trouvé pour cet utilisateur", 404);
+            return null;
+        }
+        APIResponse(response, comments, "Commentaires trouvés pour cet utilisateur");
+        return comments;
+    } catch (error) {
+        console.error("Erreur lors de la recherche des commentaires par utilisateur :", error);
+        APIResponse(response, null, "Erreur lors de la recherche des commentaires de l'utilisateur", 500);
+        return null;
+    }
+};
+
+//CRUD to get all comments by a post ID
+export const findCommentsByPostId = async (postId: Types.ObjectId, response: Response): Promise<IComment[] | null> => {
+    try {
+        const comments = await Comment.find({ postId }).exec();
+
+        if (comments.length === 0) {
+            APIResponse(response, null, "Aucun commentaire trouvé pour ce post", 404);
+            return null;
+        }
+        APIResponse(response, comments, "Commentaires trouvés pour ce post");
+        return comments;
+    } catch (error) {
+        console.error("Erreur lors de la recherche des commentaires par post :", error);
+        APIResponse(response, null, "Erreur lors de la recherche des commentaires du post", 500);
+        return null;
+    }
+};
