aef41dcde713e743aea4fb068493f1946ebea43b
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-09-30 18:08:11 +0200

    Update index.ts

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import usersRoutes from "./usersRoutes";
 import photoRoutes from "./photoRoutes";
+import postsRoutes from "./postsRoutes";
 
 const router = Router();
 
 router.use("/users", usersRoutes);
-router.use("/locations", photoRoutes);
+router.use("/photos", photoRoutes);
+router.use("/posts", postsRoutes);
 
 export default router;
