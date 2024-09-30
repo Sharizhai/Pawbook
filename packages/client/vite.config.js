9308ec4d04be036086c9a855d0edb90baad70515
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-09-30 12:21:43 +0200

    Update vite.config.js

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist/client',  // Spécifie le répertoire de sortie
+    outDir: 'dist/client',
   },
 })
