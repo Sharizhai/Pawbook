95c603df0cb4a4ac35f9b9f4e538287da2cd86f1
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-10-05 11:27:37 +0200

    likesRoutes creation + routes index update

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import usersRoutes from "./usersRoutes";
 import photoRoutes from "./photoRoutes";
 import postsRoutes from "./postsRoutes";
+import commentsRoutes from "./commentsRoutes";
+import likesRoutes from "./likesRoutes";
 
 const router = Router();
 
 router.use("/users", usersRoutes);
 router.use("/photos", photoRoutes);
 router.use("/posts", postsRoutes);
+router.use("/comments", commentsRoutes);
+router.use("/likes", likesRoutes);
 
 export default router;
