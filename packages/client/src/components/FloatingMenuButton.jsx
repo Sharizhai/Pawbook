c31ac705929abbe88a7c5bd84ee88b1d39ed22ce
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-10-08 17:06:59 +0200

    push

---
+++ packages/client/src/components/FloatingMenuButton.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import { useNavigate } from "react-router-dom";
+
+import '../css/FloatingMenu.css';
+
+const FloatingMenuButton = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default FloatingMenuButton;
