f74ce14ec7d1c9f7cf4ae32d460ffb08a39c719b
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-10-13 14:56:33 +0200

    Clean followersModel

--- packages/server/src/models/followersModel.ts
+++ packages/server/src/models/followersModel.ts
@@ -1,13 +1,7 @@
 import { Response } from "express";
 import { Types } from "mongoose";
-import { z } from "zod";
-
-import { APIResponse } from "../utils/responseUtils";
-
-import { followerValidation } from "../validation/validation";
 
 import Follower from "../schemas/followers";
-
 import { IFollower } from "../types/IFollower";
 
 //CRUD to get all followers
@@ -16,12 +10,12 @@ export const getAllFollowers = async (response: Response): Promise<IFollower[] |
         const followers = await Follower.find()
             .populate("followerUser", "name firstName profilePicture")
             .exec();
-        APIResponse(response, followers, "Liste de tous les followers récupérée avec succès");
+
         return followers;
+
     } catch (error) {
         console.error(error);
 
-        APIResponse(response, null, "Erreur lors de la récupération de la liste des followers", 500);
         return null;
     }
 };
@@ -33,15 +27,13 @@ export const findFollowerByFollowedUserId = async (id: Types.ObjectId, response:
             .populate("followerUser", "name firstName profilePicture")
             .exec();
         if (!follower) {
-            APIResponse(response, null, "Follower non trouvé", 404);
             return null;
         }
 
-        APIResponse(response, follower, "Follower trouvé");
         return follower;
     } catch (error: any) {
         console.error(error);
-        APIResponse(response, null, "Erreur lors de la recherche du follower", 500);
+
         return null;
     }
 };
@@ -49,20 +41,12 @@ export const findFollowerByFollowedUserId = async (id: Types.ObjectId, response:
 //CRUD to create a new follower
 export const createFollower = async (follower: Partial<IFollower>, response: Response): Promise<IFollower | null> => {
     try {
-        // Validation des données du follower avec Zod
-        followerValidation.parse(follower);
-
         const newFollower = await Follower.create(follower);
 
-        APIResponse(response, newFollower, "Follower créé avec succès", 201);
         return newFollower;
     } catch (error) {
-        if (error instanceof z.ZodError) {
-            APIResponse(response, error.errors, "Données du follower invalides", 400);
-        } else {
-            console.error(error);
-            APIResponse(response, null, "Erreur lors de l'ajout du follower", 500);
-        }
+        console.error(error);
+
         return null;
     }
 };
@@ -73,15 +57,13 @@ export const deleteFollower = async (id: Types.ObjectId, userId: Types.ObjectId,
         const deletedFollower = await Follower.findOneAndDelete({ _id: id, userId });
 
         if (!deletedFollower) {
-            APIResponse(response, null, "Follower non trouvé ou vous n'êtes pas autorisé à le supprimer", 404);
             return null;
         }
 
-        APIResponse(response, deletedFollower, "Follower supprimé avec succès");
         return deletedFollower;
     } catch (error) {
         console.error(error);
-        APIResponse(response, null, "Erreur lors de la suppression du follower", 500);
+
         return null;
     }
 };
@@ -92,14 +74,13 @@ export const findFollowersByUserId = async (userId: Types.ObjectId, response: Re
         const followers = await Follower.find({ userId }).exec();
 
         if (followers.length === 0) {
-            APIResponse(response, null, "Aucun follower trouvé pour cet utilisateur", 404);
             return null;
         }
-        APIResponse(response, followers, "Followers trouvés pour cet utilisateur");
+
         return followers;
     } catch (error) {
         console.error("Erreur lors de la recherche des follows par utilisateur :", error);
-        APIResponse(response, null, "Erreur lors de la recherche des followers de l'utilisateur", 500);
+
         return null;
     }
 };
