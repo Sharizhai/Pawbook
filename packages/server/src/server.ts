89dc3246307ffa0135d50e867f208d08cfe1ce99
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-10-15 18:28:02 +0200

    Update server.ts

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -12,21 +12,12 @@ const app = express();
 const allowedOrigins = ['http://localhost:5173', 'https://little-pawbook.netlify.app/'];
 
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: allowedOrigins,
   credentials: true,
   methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
 }));
 
 app.use(cookieParser());
-
-connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -36,6 +27,8 @@ app.use(Middlewares.logger);
 app.use(routes);
 
 app.use(Middlewares.error);
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running in ${NODE_ENV} mode on http://localhost:${PORT}`);
