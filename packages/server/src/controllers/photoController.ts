2383d5e7aebd63e8425944bb2c7fdaa8deb7c186
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-10-20 14:11:13 +0200

    Add logger to photoController

--- packages/server/src/controllers/photoController.ts
+++ packages/server/src/controllers/photoController.ts
@@ -6,29 +6,35 @@ import { logger, APIResponse } from "../utils";
 // Contrôleur pour l'upload d'une photo'
 export const uploadPhoto = async (req: Request, res: Response) => {
     try {
+        logger.info("[GET] /photos/singleUpload - Upload d'une photo en cours");
         // Appel du middleware d'upload pour gérer le fichier
         Middlewares.upload.single('photo')(req, res, async (err: any) => {
             if (err) {
-                return res.status(400).json({ message: err.message });
+                logger.error("Erreur lors de l'upload de la photo: " + err.message);
+                return APIResponse(res, null, err.message, 400);
             }
 
             // Appel du middleware pour mettre à jour l'entité avec les infos de la photo
             await Middlewares.storageEntity(req, res, (nextErr: any) => {
                 if (nextErr) {
-                    return res.status(500).json({ message: "Erreur lors de la mise à jour de l'entité" });
+                    logger.error("Erreur lors de la mise à jour de l'entité: " + nextErr.message);
+                    return APIResponse(res, null, "Erreur lors de la mise à jour de l'entité", 500);
                 }
                 // Réponse après la mise à jour réussie de la photo dans l'entité
-                res.status(200).json({ message: 'Entité mise à jour avec les informations de la photo' });
+                logger.info("Photo mise à jour avec succès dans l'entité");
+                return APIResponse(res, { message: 'Entité mise à jour avec les informations de la photo' }, "Photo uploadée avec succès", 200);
             });
         });
     } catch (err: any) {
-        res.status(500).json({ error: err.message });
+        logger.error("Erreur lors de l'upload de la photo: " + err.message);
+        return APIResponse(res, null, err.message, 500);
     }
 };
 
 // Méthode pour l'upload de plusieurs photos
 export const uploadMultipleFiles = async (req: Request, res: Response) => {
     try {
+        logger.info("[GET] /photos/multipleUpload - Upload d'une photo en cours");
         if (!req.files || (req.files as Express.Multer.File[]).length === 0) {
             logger.error("Aucun fichier uploadé");
             return APIResponse(res, null, "Aucun fichier uploadé", 400);
