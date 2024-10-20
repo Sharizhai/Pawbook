ae865fd282451365bb670f8e3389f60e118c6ba6
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-10-20 13:56:26 +0200

    Add loggers to postController

--- packages/server/src/controllers/postController.ts
+++ packages/server/src/controllers/postController.ts
@@ -2,20 +2,21 @@ import { Request, Response } from "express";
 import { Types } from "mongoose";
 import { z } from "zod";
 
-import { APIResponse } from "../utils/responseUtils";
+import { postValidation } from "../validation/validation";
+import { APIResponse, logger } from "../utils";
 import Model from "../models/index";
 
-import { postValidation } from "../validation/validation";
 
 //Controller pour récupérer tous les posts
 export const getPosts = async (request: Request, response: Response) => {
     try {
+        logger.info("[GET] /posts - Récupération de tous les utilisateurs");
         const posts = await Model.posts.get(response);
         
-        //Le modèle gère la réponse API. Nous retournons simplement pour terminer la fonction.
-        return;
-    } catch (error) {
-        console.error("Erreur lors de la récupération des posts:", error);
+        logger.info("Liste de tous les posts récupérée avec succès");
+        APIResponse(response, posts, "Liste de tous les posts récupérée avec succès", 200);
+    } catch (error: any) {
+        logger.error("Erreur lors de la récupération des posts: " + error.message);
         APIResponse(response, null, "Erreur lors de la récupération de la liste des posts", 500);
     }
 };
@@ -24,12 +25,19 @@ export const getPosts = async (request: Request, response: Response) => {
 export const getPostById = async (request: Request, response: Response) => {
     try {
         const id = new Types.ObjectId(request.params.id);
+        logger.info(`[GET] /posts/${id} - Récupération du post via son ID`);
+
         const post = await Model.posts.where(id, response);
 
-        //Le modèle gère la réponse API. Nous retournons simplement pour terminer la fonction.
-        return;
-    } catch (error) {
-        console.error("Erreur lors de la recherche du post :", error);
+        if (post) {
+            logger.info("Post récupéré avec succès");
+            APIResponse(response, post, "Post récupéré avec succès", 200);
+        } else {
+            logger.info("Post non trouvé");
+            APIResponse(response, null, "Post non trouvé", 404);
+        }
+    } catch (error: any) {
+        logger.error("Erreur lors de la recherche de l'utilisateur:", error.message);
         APIResponse(response, null, "Erreur lors de la recherche du post", 500);
     }
 };
@@ -37,6 +45,7 @@ export const getPostById = async (request: Request, response: Response) => {
 //Controller pour créer un nouveau post
 export const createAPost = async (request: Request, response: Response) => {
     try {
+        logger.info("[POST] /posts/register - Création d'un nouveau post");
         const postData = request.body;
 
         const validatedData = postValidation.parse(postData);
@@ -48,13 +57,14 @@ export const createAPost = async (request: Request, response: Response) => {
         };
 
         const newPost = await Model.posts.create(newPostData, response);
-
+        logger.info("Nouveau post créé avec succès");
         APIResponse(response, newPost, "Post créé avec succès", 201);
-    } catch (error) {
-        console.error("Erreur lors de la création du post :", error);
+    } catch (error: any) {
         if (error instanceof z.ZodError) {
+            logger.error("Données invalides : " + error.errors.map(e => e.message).join(", "));
             APIResponse(response, null, "Données invalides : " + error.errors.map(e => e.message).join(", "), 400);
         } else {
+            logger.error("Erreur lors de la création du post: " + error.message);
             APIResponse(response, null, "Erreur lors de la création du post", 500);
         }
     }
@@ -64,15 +74,16 @@ export const createAPost = async (request: Request, response: Response) => {
 export const deletePostById = async (request: Request, response: Response) => {
     try {
         const id = request.params.id;
+        logger.info(`[DELETE] /posts/${id} - Suppression de l'utilisateur par ID`);
         const authorId = request.params.authorId;
 
         await Model.posts.delete(new Types.ObjectId(id), new Types.ObjectId(authorId), response);
 
-        //Le modèle gère la réponse API. Nous retournons simplement pour terminer la fonction.
-        return;
-    } catch (error){
-            console.error("Erreur lors de la suppression du post :", error);
-            APIResponse(response, null, "Erreur lors de la suppression du post", 500);
+        logger.info("Post supprimé avec succès");
+        return APIResponse(response, "Post supprimé avec succès", "success", 200);
+    } catch (error : any){
+        logger.error("Erreur lors de la recherche de l'utilisateur:", error.message);
+        APIResponse(response, null, "Erreur lors de la suppression du post", 500);
     }
 };
 
@@ -80,47 +91,50 @@ export const deletePostById = async (request: Request, response: Response) => {
 export const updatePost = async (request: Request, response: Response) => {
     try {
         const id = request.params.id;
+        logger.info(`[PUT] /posts/${id} - Mise à jour de l'utilisateur`);
         const post = request.body;
 
         await Model.posts.update(new Types.ObjectId(id), post, response);
 
-        //Le modèle gère la réponse API. Nous retournons simplement pour terminer la fonction.
-        return;
-    } catch (error){
-            console.error("Erreur lors de la mise à jour du post :", error);
-            APIResponse(response, null, "Erreur lors de la mise à jour du post", 500);
+        logger.info("Post mis à jour");
+        return APIResponse(response, "Post mis à jour avec succès", "success", 200);
+    } catch (error: any){
+        logger.error("Erreur lors de la mise à jour du post :", error.message);
+        APIResponse(response, null, "Erreur lors de la mise à jour du post", 500);
     }
 };
 
 // Controller pour récupérer tous les posts d'un utilisateur spécifique
 export const getPostsByAuthorId = async (request: Request, response: Response) => {
     try {
-        console.log("Request Params:", request.params);
         const { authorId } = request.params;
+        logger.info(`[GET] /posts/user/${authorId} - Récupération du post via son authorId: ${authorId}`);
 
         if (!authorId || authorId === 'undefined') {
+            logger.warn("ID d'utilisateur manquant ou invalide");
             return APIResponse(response, null, "ID d'utilisateur manquant ou invalide", 400);
         }
 
-        console.log("User ID (string):", authorId);
-
         if (!Types.ObjectId.isValid(authorId)) {
+            logger.warn("ID d'utilisateur invalide");
             return APIResponse(response, null, "ID d'utilisateur invalide", 400);
         }
 
         const objectIdUserId = new Types.ObjectId(authorId);
-        console.log("User ID (ObjectId):", objectIdUserId);
+        logger.info("ID d'utilisateur transformé en ObjectId: " + objectIdUserId);
 
         const posts = await Model.posts.findByAuthor(objectIdUserId);
-        console.log("Posts found:", posts);
+        logger.info("Posts trouvés: " + posts!.length);
 
         if (!posts || posts.length === 0) {
+            logger.warn("Aucun post trouvé pour cet utilisateur");
             return APIResponse(response, [], "Aucun post trouvé pour cet utilisateur", 404);
         }
 
+        logger.info("Posts récupérés avec succès");
         return APIResponse(response, posts, "Posts récupérés avec succès", 200);
-    } catch (error) {
-        console.error("Erreur lors de la récupération des posts de l'utilisateur:", error);
+    } catch (error: any) {
+        logger.error("Erreur lors de la récupération des posts de l'utilisateur: " + error.message);
         APIResponse(response, null, "Erreur lors de la récupération des posts de l'utilisateur", 500);
     }
 };
