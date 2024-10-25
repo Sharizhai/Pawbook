c1690a73b6b36374d30c0230328ecf75f6de4dc6
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-10-25 21:33:19 +0200

    update follows & followers interface

--- packages/server/src/types/IFollow.d.ts
+++ packages/server/src/types/IFollow.d.ts
@@ -3,7 +3,7 @@ import { Document, Types } from "mongoose";
 import { IUser } from "./IUser";
 
 export interface IFollow extends Document {
-    userID: Types.ObjectId;  // L'utilisateur qui suit
-    followedUser: IUser;  // L'utilisateur qui est suivi
+    followerUser: Types.ObjectId;  // L'utilisateur qui suit
+    followedUser: Types.ObjectId;  // L'utilisateur qui est suivi
     createdAt: Date;
 }
