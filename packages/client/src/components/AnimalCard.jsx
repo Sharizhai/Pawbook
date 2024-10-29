aa2162acede8e7c8b550b88b9772984ec5d9f042
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-10-29 12:15:26 +0100

    fix animalcard

--- packages/client/src/components/AnimalCard.jsx
+++ packages/client/src/components/AnimalCard.jsx
@@ -5,27 +5,29 @@ import '../css/AnimalCard.css';
 
 const AnimalCard = ({ animal }) => {
     return (
-      <div className="animal-card-container">
-        <SettingsButton className="animal-card-settings-button" />
-        <div className="animal-card-photo-and-resume">
-          <div className="animal-card-profil-picture-container">
-            <img
-              src={animal.picture || Animal_profil_pic}
-              alt={`Image de ${animal.name}`}
-              className="animal-card-profil-picture"
-            />
-          </div>
-          <div className="animal-card-resume-container">
-            <h3 className="animal-card-name-label">{animal.name}</h3>
-            <h4 className="animal-card-infos-label">
-              {`${animal.race}, ${animal.type}${animal.age ? `, ${animal.age} ans` : ''}`}
-            </h4>
-            <p className="animal-card-bio-text">{animal.description}</p>
-          </div>
+        <div className="animal-card-container">
+            <SettingsButton className="animal-card-settings-button" />
+            <div className="animal-card-photo-and-resume">
+                <div className="animal-card-profil-picture-container">
+                    <img
+                        src={animal.picture || Animal_profil_pic}
+                        alt={`Image de ${animal.name}`}
+                        className="animal-card-profil-picture"
+                    />
+                </div>
+                <div className="animal-card-resume-container">
+                    <h3 className="animal-card-name-label">{animal.name}</h3>
+                    <h4 className="animal-card-infos-label">
+                        {`${animal.race}, ${animal.type}${animal.age ? `, ${animal.age} ans` : ''}`}
+                    </h4>
+                    <p className="animal-card-bio-text">{animal.description}</p>
+                </div>
+            </div>
+            <button className="animal-card-like-button">
+                <span className="material-symbols-outlined">favorite</span>
+            </button>
         </div>
-        <button className="animal-card-like-button">
-          <span className="material-symbols-outlined">favorite</span>
-        </button>
-      </div>
     );
-  };
+};
+
+export default AnimalCard;
