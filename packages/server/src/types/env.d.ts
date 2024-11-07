c344b6bb167f191320ad1a0ab887215f6501696f
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-11-07 09:32:15 +0100

    Update env.d.ts

--- packages/server/src/types/env.d.ts
+++ packages/server/src/types/env.d.ts
@@ -2,7 +2,13 @@ export interface IEnv {
     PORT: number;
     NODE_ENV: "development" | "production" | "test";
     JWT_SECRET: string;
-    JWT_RESET_PWD_SECRET:string;
+    JWT_RESET_PWD_SECRET: string;
+    FRONTEND_URL: string;
     ORIGIN: string;
     MONGO_URI: string;
+    CLOUDINARY_URL: string;
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
+    LOGO_URL: string;
 }
