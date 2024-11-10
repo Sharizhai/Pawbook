b2d403aed73212ac212ad393dd28a1ec240fc4bf
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-11-11 00:00:03 +0100

    env update

--- packages/server/src/config/env.ts
+++ packages/server/src/config/env.ts
@@ -7,6 +7,20 @@ export const env: IEnv = {
     PORT: parseInt(process.env.PORT || "3000"),
     NODE_ENV: process.env.NODE_ENV as 'development' | 'production' | 'test',
     JWT_SECRET: process.env.JWT_SECRET || "¡SecretSecret!",
+    JWT_EXPIRATION_SECRET: process.env.JWT_EXPIRATION_SECRET || "¡SecretSecret!",
+    FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:3000",
     ORIGIN: process.env.ORIGIN || "http://localhost:5173",
-    MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/express"
+    MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/express",
+
+    // nodemailer
+    EMAIL_USER: process.env.EMAIL_USER || "mail@mail.mail",
+    EMAIL_APP_PASSWORD: process.env.EMAIL_APP_PASSWORD || "¡SecretSecret!",
+    JWT_RESET_PWD_SECRET: process.env.JWT_RESET_PWD_SECRET || "¡SecretSecret!",
+
+    // Cloudinary
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL || "cloudinary://<your_api_key>:<your_api_secret>@<petit_nuage_fondant>",
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || "petit_nuage_fondant",
+    CLOUDINARY_API_KEY: process.env.MONGO_URI || "your_api_key",
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || "your_api_secret",
+    LOGO_URL: process.env.LOGO_URL || "¡SecretSecret!",
 };
--- packages/server/src/types/env.d.ts
+++ packages/server/src/types/env.d.ts
@@ -2,10 +2,17 @@ export interface IEnv {
     PORT: number;
     NODE_ENV: "development" | "production" | "test";
     JWT_SECRET: string;
-    JWT_RESET_PWD_SECRET: string;
+    JWT_EXPIRATION_SECRET: string;
     FRONTEND_URL: string;
     ORIGIN: string;
     MONGO_URI: string;
+
+    // Nodemailer
+    EMAIL_USER: string;
+    EMAIL_APP_PASSWORD: string;
+    JWT_RESET_PWD_SECRET: string;
+
+    // Cloudinary
     CLOUDINARY_URL: string;
     CLOUDINARY_CLOUD_NAME: string;
     CLOUDINARY_API_KEY: string;
