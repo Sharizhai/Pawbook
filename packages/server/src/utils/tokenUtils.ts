c985e6bf14168b72b287a9e7b4903f16dee33afc
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-11-28 23:05:50 +0100

    Update tokenUtils.ts

--- packages/server/src/utils/tokenUtils.ts
+++ packages/server/src/utils/tokenUtils.ts
@@ -3,15 +3,15 @@ import { env } from "../config/env";
 
 const { JWT_SECRET, JWT_EXPIRATION_SECRET, REFRESH_TOKEN_SECRET, REFRESH_TOKEN_EXPIRATION_SECRET } = env;
 
-// On crée un token d'accès
-export const createAccessToken = (userId: string): string => {
-    return jwt.sign({ id: user._id, email: user.email }, JWT_SECRET, { expiresIn: JWT_EXPIRATION_SECRET});
-};
+// // On crée un token d'accès
+// export const createAccessToken = (userId: string): string => {
+//     return jwt.sign({userId}, JWT_SECRET, { expiresIn: JWT_EXPIRATION_SECRET});
+// };
 
-// On crée un token de rafraîchissement
-export const createRefreshToken = (userId: string): string => {
-    return jwt.sign({userId}, REFRESH_TOKEN_SECRET, { expiresIn: REFRESH_TOKEN_EXPIRATION_SECRET});
-};
+// // On crée un token de rafraîchissement
+// export const createRefreshToken = (userId: string): string => {
+//     return jwt.sign({userId}, REFRESH_TOKEN_SECRET, { expiresIn: REFRESH_TOKEN_EXPIRATION_SECRET});
+// };
 
 // On vérifie le token de rafraîchissement
 export const verifyRefreshToken = (token: string): string | null => {
