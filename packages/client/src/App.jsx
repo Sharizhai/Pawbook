92c378eea56cc81d06fa6fba0ae39a7e92b4320b
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-12-04 13:14:10 +0100

    Update App.jsx

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -5,12 +5,17 @@ import AppRoutes from './routes/Routes.jsx';
 const App = () => {
 
     // Bloque React dev tools en production
-    if (process.env.NODE_ENV === 'production') {
-        if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
-            window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = () => {};
-            window.__REACT_DEVTOOLS_GLOBAL_HOOK__.reactDevtoolsAgent = null;
+    React.useEffect(() => {
+        if (process.env.NODE_ENV === 'production') {
+            if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ !== 'undefined') {
+                for (const prop in window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+                    if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] === 'function') {
+                        window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = () => {};
+                    }
+                }
+            }
         }
-    }
+    }, []);
 
     return (
         <>
