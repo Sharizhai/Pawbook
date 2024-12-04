722bed796964d5e291f694a266ccebef0c1af0e2
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-12-04 11:12:19 +0100

    update packages + App

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/Routes.jsx';
 
 const App = () => {
+    if (process.env.NODE_ENV === 'production') {
+        window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = {};
+    }
+
     return (
         <>
             <Router>
