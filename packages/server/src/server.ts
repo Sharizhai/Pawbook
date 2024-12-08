70eed2056aa3a9030c5371d7d15957a51f5a8802
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-12-08 10:08:05 +0100

    csp

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -50,6 +50,11 @@ app.use(helmet({
         "cloudinary.com",
         `https://${process.env.FRONTEND_DOMAIN}`
       ],
+      connectSrc: [
+        "'self'",
+        `https://${process.env.FRONTEND_DOMAIN}`,
+        `${process.env.BACKEND_URL}`
+      ],
       frameSrc: ["'none'"],
       upgradeInsecureRequests: [],
       blockAllMixedContent: []
