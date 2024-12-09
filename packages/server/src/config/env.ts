228a150f1a0f83158dfd8ceecd603d01fc64ab3a
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-12-09 14:58:56 +0100

    Revert "Test en prod"
    This reverts commit 5ea4a553d58a91f918b11eb99380f422de8640d8.

--- packages/server/src/config/env.ts
+++ packages/server/src/config/env.ts
@@ -7,62 +7,28 @@ export const env: IEnv = {
     PORT: parseInt(process.env.PORT || "3000"),
     NODE_ENV: process.env.NODE_ENV as 'development' | 'production' | 'test',
 
-    JWT_SECRET: process.env.JWT_SECRET || (() => {
-        throw new Error('JWT_SECRET must be defined');
-    })(),
-    JWT_EXPIRATION_SECRET: process.env.JWT_EXPIRATION_SECRET || (() => {
-        throw new Error('JWT_EXPIRATION_SECRET must be defined');
-    })(),
-    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || (() => {
-        throw new Error('REFRESH_TOKEN_SECRET must be defined');
-    })(),
-    REFRESH_TOKEN_EXPIRATION_SECRET: process.env.REFRESH_TOKEN_EXPIRATION_SECRET || (() => {
-        throw new Error('REFRESH_TOKEN_EXPIRATION_SECRET must be defined');
-    })(),
+    JWT_SECRET: process.env.JWT_SECRET || "¡SecretSecret!123",
+    JWT_EXPIRATION_SECRET: process.env.JWT_EXPIRATION_SECRET || "30m",
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || "¡SecretSecret!321",
+    REFRESH_TOKEN_EXPIRATION_SECRET: process.env.REFRESH_TOKEN_EXPIRATION_SECRET || "2j",
 
-    FRONTEND_URL: process.env.FRONTEND_URL || (() => {
-        throw new Error('FRONTEND_URL must be defined');
-    })(),
-    ORIGIN: process.env.ORIGIN || (() => {
-        throw new Error('ORIGIN must be defined');
-    })(),
-    MONGO_URI: process.env.MONGO_URI || (() => {
-        throw new Error('MONGO_URI must be defined');
-    })(),
+    FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:3000",
+    ORIGIN: process.env.ORIGIN || "http://localhost:5173",
+    MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/express",
 
     // nodemailer
-    EMAIL_USER: process.env.EMAIL_USER || (() => {
-        throw new Error('EMAIL_USER must be defined');
-    })(),
-    EMAIL_APP_PASSWORD: process.env.EMAIL_APP_PASSWORD || (() => {
-        throw new Error('EMAIL_APP_PASSWORD must be defined');
-    })(),
-    JWT_RESET_PWD_SECRET: process.env.JWT_RESET_PWD_SECRET || (() => {
-        throw new Error('JWT_RESET_PWD_SECRET must be defined');
-    })(),
+    EMAIL_USER: process.env.EMAIL_USER || "mail@mail.mail",
+    EMAIL_APP_PASSWORD: process.env.EMAIL_APP_PASSWORD || "¡SecretSecret!",
+    JWT_RESET_PWD_SECRET: process.env.JWT_RESET_PWD_SECRET || "¡SecretSecret!",
 
     // Cloudinary
-    CLOUDINARY_URL: process.env.CLOUDINARY_URL || (() => {
-        throw new Error('CLOUDINARY_URL must be defined');
-    })(),
-    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || (() => {
-        throw new Error('CLOUDINARY_CLOUD_NAME must be defined');
-    })(),
-    CLOUDINARY_API_KEY: process.env.MONGO_URI || (() => {
-        throw new Error('MONGO_URI must be defined');
-    })(),
-    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || (() => {
-        throw new Error('CLOUDINARY_API_SECRET must be defined');
-    })(),
-    LOGO_URL: process.env.LOGO_URL || (() => {
-        throw new Error('LOGO_URL must be defined');
-    })(),
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL || "cloudinary://<your_api_key>:<your_api_secret>@<petit_nuage_fondant>",
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || "petit_nuage_fondant",
+    CLOUDINARY_API_KEY: process.env.MONGO_URI || "your_api_key",
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || "your_api_secret",
+    LOGO_URL: process.env.LOGO_URL || "¡SecretSecret!",
 
     // Helmet
-    FRONTEND_DOMAIN: process.env.FRONTEND_DOMAIN || (() => {
-        throw new Error('FRONTEND_DOMAIN must be defined');
-    })(),
-    BACKEND_URL: process.env.BACKEND_URL || (() => {
-        throw new Error('BACKEND_URL must be defined');
-    })(),
+    FRONTEND_DOMAIN: process.env.FRONTEND_DOMAIN || "https://yourfront.com",
+    BACKEND_URL: process.env.BACKEND_URL || "https://yourback.com",
 };
