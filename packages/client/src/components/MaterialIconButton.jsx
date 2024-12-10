65ace85fbafbf9217e5340024b94e4f148c2311a
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-12-10 14:08:38 +0100

    Update MaterialIconButton.jsx

--- packages/client/src/components/MaterialIconButton.jsx
+++ packages/client/src/components/MaterialIconButton.jsx
@@ -18,17 +18,9 @@ const MaterialIconButton = ({
             if (onOpenMenu) onOpenMenu();
         } else if (iconName === "close") {
             if (onClosePanel) onClosePanel();
-        } else if (iconName === "photo_library") {
-            fileInputRef.current.click();
         }
+        
         if (onClick) onClick();
-    };
-
-    const handleFileChange = (event) => {
-        const files = event.target.files;
-        if (files && files.length > 0 && onPhotoSelect) {
-            onPhotoSelect(files);
-        }
     };
 
     return (
@@ -39,14 +31,6 @@ const MaterialIconButton = ({
                         {iconName}
                     </span>
                     <span className="add-photos-text">Ajouter des photos</span>
-                    <input 
-                        type="file"
-                        ref={fileInputRef}
-                        style={{ display: 'none' }}
-                        onChange={handleFileChange}
-                        accept="image/*"
-                        multiple
-                    />
                 </button>
             ) : (
                 <span
