51dba82b843de744a5924aaf282ac3743d30e6c3
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-12-13 23:13:54 +0100

    add icon for password visibility in input

--- packages/client/src/components/Input.jsx
+++ packages/client/src/components/Input.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/Input.css';
 
 const Input = ({ label, type = 'text', name, value, onChange, placeholder, onFocus }) => {
+  const [inputType, setInputType] = useState(type);
+
+  const handleToggle = () => {
+    setInputType(prevType => (prevType === 'password' ? 'text' : 'password'));
+  }
+
   return (
     <div className="input-container">
       <label className="input-label" htmlFor={name}>{label}</label>
-      <input
-        className="input-field"
-        type={type}
-        id={name}
-        name={name}
-        value={value}
-        onChange={onChange}
-        onFocus ={onFocus}
-        placeholder={placeholder}
-      />
+      <div className="input-wrapper">
+        <input
+          className="input-field"
+          type={inputType}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          onFocus={onFocus}
+          placeholder={placeholder}
+        />
+        {type === "password" && (
+          <span className="input-password-icon material-symbols-outlined"
+                onClick={handleToggle}>
+            {inputType === 'password' ? 'visibility' : 'visibility_off'}
+          </span>
+        )}
+      </div>
     </div>
   );
 };
