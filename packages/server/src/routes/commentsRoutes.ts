0068ccd41fa8b948456ee791613a60a7762159fd
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-12-13 09:05:53 +0100

    little fixes

--- packages/server/src/routes/commentsRoutes.ts
+++ packages/server/src/routes/commentsRoutes.ts
@@ -16,6 +16,6 @@ router.get("/:postId", Controllers.comments.getByPostId);
 //Routes delete et update pour un admin
 router.get("/admin/:authorId", Middlewares.isAdmin, Controllers.comments.adminGetByUserId);
 router.delete("/admin/:postId/:commentId/:authorId", Middlewares.isAdmin, Controllers.comments.delete);
-router.put("/admin/:id", Middlewares.isAdmin, Middlewares.validationComment, Controllers.comments.adminUpdate);
+router.put("/admin/:id", Middlewares.isAdmin, Middlewares.validationCommentAdmin, Controllers.comments.adminUpdate);
 
 export default router;
