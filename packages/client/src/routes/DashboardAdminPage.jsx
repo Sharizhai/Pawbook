387120c96ccdc07f41bdb736a04ce96e47e3dfcb
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-12-13 20:53:56 +0100

    Update DashboardAdminPage.jsx

--- packages/client/src/routes/DashboardAdminPage.jsx
+++ packages/client/src/routes/DashboardAdminPage.jsx
@@ -93,11 +93,8 @@ const DashboardAdminPage = () => {
                 });
                 
                 if (!response.ok) {
-                    // console.error("Erreur lors de la récupération de tous les posts");
-                    // return;
-                    const errorText = await response.text();
-            console.error("Détails de l'erreur :", errorText);
-            throw new Error(`Erreur HTTP: ${response.status}`);
+                    console.error("Erreur lors de la récupération de tous les posts");
+                    return;
                 }
                 
                 const postList = await response.json();
